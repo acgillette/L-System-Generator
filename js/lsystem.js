9cc471f680ce4aa6d868364e47244bd6b08eb2ee
Anacelia Gillette <acgillette426@gmail.com>
2017-08-24 17:49:24 -0700

    Implemented L System class, started building GUI

--- js/lsystem.js
+++ js/lsystem.js
@@ -1,9 +1,56 @@
-
 class LSystem {
-  constructor() {
-
+  constructor(data) {
+    this.axiom = data.axiom;
+    this.angle = radians(data.angle);
+    this.n = data.n;
+    this.rules = data.rules;
+    this.sentence = this.axiom;
+    this.len = data.len;
   }
-  test() {
-    console.log("Here I am");
+
+  generateSentence() {
+    var nextSentence = "";
+    for(var i = 0; i < this.n; i++) {
+        for(var j = 0; j < this.sentence.length; j++) {
+          var current = this.sentence.charAt(j);
+          var found = false;
+
+          for(var k = 0; k < this.rules.length; k++) {
+            if (current == this.rules[k].a) {
+              found = true;
+              nextSentence += this.rules[k].b;
+              break;
+            }
+          }
+
+          if(!found) {
+            nextSentence += current;
+          }
+        }
+        this.sentence = nextSentence;
+        this.drawSystem();
+        this.len *= 0.25;
+      }
+  }
+
+  drawSystem() {
+    background(51);
+    resetMatrix();
+    translate(width/4, height/1.30);
+    stroke(255);
+    for(var i = 0; i < this.sentence.length; i++) {
+      var current = this.sentence.charAt(i);
+
+      if(current === "F" || current == "R") {
+        line(0, 0, 0, -this.len);
+        translate(0, -this.len);
+      } else if (current === "+") {
+        rotate(this.angle);
+      } else if (current === "-") {
+        rotate(-this.angle);
+      } else if (current === "f") {
+        translate(0, -this.len);
+      }
+    }
   }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,44 @@
+var gui;
+var systems = ['Koch Island', 'Koch Triangle', 'Koch Islands and Lakes', 'Gosper Curve'];
+var data = {
+  axiom: "F-F-F-F",
+  angle: 90,
+  rules: [
+    ruleOne = {
+      a: "F",
+      b: "F+FF-FF-F-F+F+FF-F-F+F+FF+FF-F"
+    }
+  ],
+  len: 50,
+  n: 2
+};
+
+
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
   background(51);
-  var system = new LSystem();
-  system.test();
 
+
+  gui = createGui('L-System Visualizer');
+  gui.addGlobals('systems');
+
+  noLoop();
 }
 
 function draw() {
-  
+  background(51);
+  switch(systems) {
+    case 'Koch Island':
+      var system = new LSystem(data);
+      system.generateSentence();
+      break;
+    default:
+      console.log("Not implemented");
+      break;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
