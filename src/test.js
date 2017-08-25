6aab525fa8f324b35f455d1ba93296caecdf6634
Anacelia Gillette <acgillette426@gmail.com>
2017-08-25 00:34:39 -0700

    Starting from scratch with Three.js

---
+++ src/test.js
@@ -0,0 +1,18 @@
+var THREE = require('three');
+
+export default class Test {
+  constructor() {
+    this.material = new THREE.LineBasicMaterial({
+  	color:  0xffffff
+    });
+
+  this.geometry = new THREE.Geometry();
+  this.geometry.vertices.push(
+    new THREE.Vector3( 0, -5, -7 ),
+    new THREE.Vector3( 0, 5, -7 )
+  );
+
+  this.line = new THREE.Line( this.geometry, this.material );
+
+  }
+}
